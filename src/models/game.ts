0414acaf3f08c86445c1ad761cd76e9b631a621c
Dennis-Schneider <107464538+Dennis-Schneider@users.noreply.github.com>
2023-04-23 21:34:39 +0200

    11 | Alle Spieler anzeigen

--- src/models/game.ts
+++ src/models/game.ts
@@ -1,5 +1,5 @@
 export class Game {
-  public players: string[] = [];
+  public players: string[] = ['Dennis', 'Heiko', 'Nora'];
   public stack: string[] = [];
   public playedCards: string[] = [];
   public currentPlayer: number = 0;
