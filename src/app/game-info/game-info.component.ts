e497696946c37e6e4c66017911c6f4c4f24950d3
Dennis-Schneider <107464538+Dennis-Schneider@users.noreply.github.com>
2023-04-25 22:14:25 +0200

    18 | Spielregeln aus JSON laden

--- src/app/game-info/game-info.component.ts
+++ src/app/game-info/game-info.component.ts
@@ -1,10 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-game-info',
   templateUrl: './game-info.component.html',
-  styleUrls: ['./game-info.component.scss']
+  styleUrls: ['./game-info.component.scss'],
 })
 export class GameInfoComponent {
+  cardAction = [
+    {
+      title: 'Waterfall',
+      description:
+        'Everyone has to start drinking at the same time. As soon as player 1 stops drinking, player 2 may stop drinking. Player 3 may stop as soon as player 2 stops drinking, and so on.',
+    },
+    { title: 'You', description: 'You decide who drinks' },
+    { title: 'Me', description: 'Congrats! Drink a shot!' },
+    {
+      title: 'Category',
+      description:
+        'Come up with a category (e.g. Colors). Each player must enumerate one item from the category.',
+    },
+    {
+      title: 'Bust a jive',
+      description:
+        'Player 1 makes a dance move. Player 2 repeats the dance move and adds a second one. ',
+    },
+    { title: 'Chicks', description: 'All girls drink.' },
+    {
+      title: 'Heaven',
+      description: 'Put your hands up! The last player drinks!',
+    },
+    {
+      title: 'Mate',
+      description:
+        'Pick a mate. Your mate must always drink when you drink and the other way around.',
+    },
+    {
+      title: 'Thumbmaster',
+      description:
+        'The player who drew the card says a word, and you go around the circle rhyming with that word until someone messes up, and has to drink.',
+    },
+    { title: 'Men', description: 'All men drink.' },
+    {
+      title: 'Quizmaster',
+      description:
+        " Make a rule. The player who drew the card makes a new rule (e.g. you can't say the word 'yes' or you can't put your drink down) and anyone who breaks the rule at any time throughout the rest of the game has to drink.",
+    },
+    {
+      title: 'Never have i ever...',
+      description:
+        'Say something you nnever did. Everyone who did it has to drink.',
+    },
+    {
+      title: 'Rule',
+      description:
+        'Make a rule. Everyone needs to drink when he breaks the rule.',
+    },
+  ];
 
+  title = '';
+  description = '';
+  @Input() card: string;
+
+  ngOnInit(): void {}
+
+  ngOnChanges(): void {
+    if (this.card) {
+      console.log('Current card: ' + this.card);
+      let cardNumber = +this.card.split('_')[1];
+      this.title = this.cardAction[cardNumber - 1].title;
+      this.description = this.cardAction[cardNumber - 1].description;
+    }
+  }
 }
