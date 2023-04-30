7c72828e3ff7cb9364e24333f3d4d8d863bb7e4f
Dennis-Schneider <107464538+Dennis-Schneider@users.noreply.github.com>
2023-04-30 22:45:44 +0200

    14 | 2 | 05 - bis 3:50

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'ds-ring-of-fire',
+    appId: '1:470202109315:web:07b5ee9c4afe3c82fb7ab6',
+    storageBucket: 'ds-ring-of-fire.appspot.com',
+    apiKey: 'AIzaSyAwsZdIlXcxFbNIl99p2L_qHxzQXHBTKbQ',
+    authDomain: 'ds-ring-of-fire.firebaseapp.com',
+    messagingSenderId: '470202109315',
+  },};
